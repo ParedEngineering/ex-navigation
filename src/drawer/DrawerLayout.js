53d96908f4ccdb2fd9daea5522d28df88d203fae
Sunny Qin <sunny@interactivelabs.co>
2018-02-12 14:32:59 +0800

    for pared

--- src/drawer/DrawerLayout.js
+++ src/drawer/DrawerLayout.js
@@ -427,7 +427,7 @@ const styles = StyleSheet.create({
         position: 'absolute',
         top: isIPhoneX ? -44 : 0,
         left: 0,
-        bottom: 0,
+        bottom: isPhoneX ? -34 : 0,
         right: 0,
         zIndex: 1000,
     },
